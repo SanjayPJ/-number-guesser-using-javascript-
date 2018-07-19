33c4c733f93bf0e3dad53bc82b6671ceb59d9eb2
sanjay.pj <sanjaypjayan2000@gmail.com>
2018-07-19 17:53:53 +0530

    play again fixed

--- app.js
+++ app.js
@@ -18,33 +18,37 @@ const message = document.querySelector('.message');
 minimumNumber.textContent = min;
 maximumNumber.textContent = max;
 
-gussButton.addEventListener('click',function(){
+gussButton.addEventListener('click', function () {
   var guss = parseInt(gussNumber.value);
-  if(isNaN(guss) || guss < min || guss > max){
-    setMessage(`Please enter a number between ${min} and ${max}`,'red');
-  }else{
-    if(guss === winningNumber){
-      gussNumber.disabled  = true;
-      gussNumber.style.borderColor = "green";
-      setMessage(`${winningNumber} is correct, YOU WIN!`, 'green')
-    }else{
-      gussesLeft -= 1;
-      if(gussesLeft == 0){
-        setMessage(`Currect number is ${winningNumber},YOU LOSE`,'red');
-      }else{
-        setMessage(` ${gussNumber.value} is Wrong, you have ${gussesLeft} guess left.`,'red');
+  if (isNaN(guss) || guss < min || guss > max) {
+    setMessage(`Please enter a number between ${min} and ${max}`, 'red');
+  } else {
+    if(document.querySelector('.guess-btn').value !== "PLAY AGAIN"){
+      if (guss === winningNumber) {
+        gussNumber.style.borderColor = "green";
+        setMessage(`${winningNumber} is correct, YOU WIN!`, 'green');
+        document.querySelector('.guess-btn').value = "PLAY AGAIN";
+      } else {
+        gussesLeft -= 1;
+        if (gussesLeft == 0) {
+          setMessage(`Currect number is ${winningNumber},YOU LOSE`, 'red');
+          gussNumber.style.borderColor = "red";
+          document.querySelector('.guess-btn').value = "PLAY AGAIN";
+        } else {
+          setMessage(` ${gussNumber.value} is Wrong, you have ${gussesLeft} guess left.`, 'red');
+          gussNumber.style.borderColor = "red";
+        }
       }
+    }else{
+      gussNumber.style.borderColor = "grey";
+      message.textContent = "";
+      document.querySelector('.guess-btn').value = "SUBMIT";
+      gussNumber.value = "";
+      gussesLeft = 3
     }
   }
 });
-
-
-function setMessage(messageDemo,colour){
+function setMessage(messageDemo, colour) {
   message.style.color = colour;
   message.textContent = messageDemo;
-}
-function reset(){
-  gussNumber.disabled  = false;
-  gussNumber.style.borderColor = "grey";
-  setMessage(`${winningNumber} is correct, YOU WIN!`, 'green')
 }
